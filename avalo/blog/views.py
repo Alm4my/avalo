1b6df2cc0a96cbf52c4775066dcf71fd64422ac0
Alm4my <thenixadm@gmail.com>
2021-10-13 05:51:24 +0000

    Remove class for view post_list

--- avalo/blog/views.py
+++ avalo/blog/views.py
@@ -1,17 +1,9 @@
-# from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.core.mail import send_mail
+from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
 
 from blog.forms import EmailPostForm, CommentForm
 from blog.models import Post
-
-
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
 
 
 def post_share(request, post_id):
@@ -68,22 +60,23 @@ def post_detail(request, year, month, day, post):
                                                      'new_comment': new_comment,
                                                      'comment_form': comment_form})
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)  # 3 posts per page
-#     page = request.GET.get('page')
-#
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         # if page is not an integer deliver the first page
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # if page is out of range deliver last page of results
-#         posts = paginator.page(paginator.num_pages)
-#
-#     return render(request, 'blog/post/list.html', {'page': page,
-#                                                    'posts': posts})
+
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 posts per page
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                   'posts': posts})
 
 
 
