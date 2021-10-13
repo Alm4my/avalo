01f32ab38549b6f09c33df0b8b5511ab7bb0fbec
Alm4my <thenixadm@gmail.com>
2021-10-13 04:38:58 +0000

    Register the model in the admin site

--- avalo/blog/admin.py
+++ avalo/blog/admin.py
@@ -1,10 +1,11 @@
+from django.contrib.admin import ModelAdmin
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Comment
 
 
 # Register your models here.
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body', 'author')
@@ -12,3 +13,11 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
+
+
+@admin.register(Comment)
+class CommentAdmin(ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
