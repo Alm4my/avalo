be74848d2a1a806b3911a56fe8c6be6d0c329fd6
Alm4my <thenixadm@gmail.com>
2021-10-13 01:12:41 +0000

    Replace function-based view by class-based view for posts list

--- avalo/blog/views.py
+++ avalo/blog/views.py
@@ -1,25 +1,33 @@
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+# from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from blog.models import Post
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)  # 3 posts per page
-    page = request.GET.get('page')
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # if page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # if page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'blog/post/list.html', {'page': page,
-                                                   'posts': posts})
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3)  # 3 posts per page
+#     page = request.GET.get('page')
+#
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # if page is not an integer deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # if page is out of range deliver last page of results
+#         posts = paginator.page(paginator.num_pages)
+#
+#     return render(request, 'blog/post/list.html', {'page': page,
+#                                                    'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
