d17f6abbf98bcd92a2a6487a4535c6bf0ed40c1e
Alm4my <thenixadm@gmail.com>
2021-10-13 04:33:01 +0000

    Add comments model

--- avalo/blog/models.py
+++ avalo/blog/models.py
@@ -44,3 +44,19 @@ class Post(Model):
 
     objects = models.Manager()  # default manager
     published = PublishedManager()  # custom manager
+
+
+class Comment(Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'comment by {self.name} on {self.post}'
